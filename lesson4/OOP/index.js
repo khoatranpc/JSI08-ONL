45357a55c731638ac7ffdfbb46a7c13da88c48b1
Trần Đăng Khoa <khoatranpc603@gmail.com>
2022-05-22 10:17:36 +0700

    less 5, register với localStorage

--- lesson4/OOP/index.js
+++ lesson4/OOP/index.js
@@ -10,13 +10,15 @@ class Form {
     }
     createAttributes() {
         this.node.className = "form-sigup";
-        this.h1.innerText="Đăng ký"
+        this.h1.innerText = "Đăng ký"
         this.inputEmail.name = "email";
         this.inputEmail.type = "email";
 
         this.inputPassword.type = "password";
         this.inputPassword.name = "password";
-        this.button.innerText="Register"
+        this.button.innerText = "Register"
+
+        this.button.type = 'submit'
     }
     createForm() {
         this.createAttributes();
@@ -31,9 +33,51 @@ class Form {
 
         //khởi tạo innerhtml cho node
         this.root.appendChild(this.node);
+
+    }
+    register(email, password) {
+        try {
+            if (email == '') throw new Error("Email must be not empty!");
+            if (password == '' || password.length <= 6)
+                throw new Error("Password must be not empty and large 6 character!");
+            // code...
+            const account = {
+                email: email,
+                password: password
+            }
+            let listAccount;
+            if (localStorage.getItem('listAccount'))
+                listAccount = JSON.parse(localStorage.getItem('listAccount'))
+            else listAccount = new Array();
+            // sử dụng filter để lọc ra các email đã tồn tại,
+            // kết quả trả về của filter luôn có độ rộng bằng với số lượng các giá trị thỏa mãn điều kiện
+            const checkExistedEmail = listAccount.filter((item, index) => {
+                return item.email == email;
+            })
+            if (checkExistedEmail.length != 0) throw new Error('Email already existed!')
+
+            listAccount.push(account)
+
+            localStorage.setItem('listAccount', JSON.stringify(listAccount));
+
+
+        } catch (error) {
+            alert(error.message);
+        }
     }
 }
 let createFormRegister = new Form();
-
+console.log('đây là oop');
 createFormRegister.createForm();
+
 document.querySelector('div.container').innerHTML += createFormRegister.root.innerHTML;
+
+let form = document.querySelector('form');
+
+let email = form['email'];
+let password = form['password'];
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    createFormRegister.register(email.value, password.value);
+})
